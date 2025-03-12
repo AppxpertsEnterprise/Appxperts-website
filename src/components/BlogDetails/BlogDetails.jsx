153fee09e26059a618be31ed972f0b38ceb9c037
Maheswari B <appxperts.enterprise.solution@gmail.com>
2025-03-12 16:10:52 +0530

    nav change

--- src/components/BlogDetails/BlogDetails.jsx
+++ src/components/BlogDetails/BlogDetails.jsx
@@ -136,7 +136,7 @@ const BlogDetails = () => {
                                     </h5>
                                     <div className="blog-details__blockquote__meta__social">
                                         {blockquoteAuthor.social.map((link, index) => (
-                                            <Link key={index} href={link.href}>
+                                            <Link key={index} href={link.href} target="_blank" rel="noopener noreferrer">
                                                 <i className={link.icon} aria-hidden="true"></i>
                                                 <span className="sr-only">{link.name}</span>
                                             </Link>
