40b5f1515f71367962a571fe691a748b168f9f17
Maheswari B <maheswarib.appxperts@gmail.com>
2025-03-26 14:59:56 +0530

    recent fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "54683aacf46918d6"; }
+/******/ 		__webpack_require__.h = function() { return "58d4882913f8b80e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
